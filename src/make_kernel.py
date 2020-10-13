a2504b0063d3186c451910ac9b019f194336586e
jerefarrher <jerefarrher91@gmail.com>
2020-10-13 11:16:18 -0300

    added NLM_filter

---
+++ src/make_kernel.py
@@ -0,0 +1,17 @@
+#funcion kernel 
+import numpy as np
+
+def make_kernel(f):              
+
+"""
+crea la ventana cuadrada móvil local de lado 2f+1
+"""
+    kernel=np.zeros((2*f+1,2*f+1))  
+    for d in range(1,f+1):    
+        value= 1 / (2*d+1)**2  
+        for i in range(-d,d+1):
+            for j in range(-d,d+1):
+                kernel[f+1-i-1,f+1-j-1]= kernel[f+1-i-1,f+1-j-1] + value 
+     
+    kernel = kernel / f
+    return kernel
