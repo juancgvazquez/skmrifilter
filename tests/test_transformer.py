14d64a6344faecc1810c1aaed0797584b28cc8b4
Charly <juancgvazquez@gmail.com>
2020-11-03 11:37:25 -0300

    changed assert for numpy assert

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -12,7 +12,7 @@ from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
 from skmrifilter.transformer import ImageFilterTransformer
 from sklearn.pipeline import Pipeline
 import pickle
-import numpy as np
+import numpy.testing as npt
 
 
 def test_simple_transformer():
@@ -22,7 +22,8 @@ def test_simple_transformer():
     transformed_image = transformer.transform(noisy)
     with open('tests/test_output1.pkl', 'rb') as f:
         test_transformer = pickle.load(f)
-    assert np.array_equal(test_transformer, transformed_image)
+    npt.assert_array_almost_equal(test_transformer, transformed_image,
+                                         decimal=15)
 
 
 def test_pipeline_transformer():
@@ -36,4 +37,5 @@ def test_pipeline_transformer():
     pipe_transformed = pipe.transform(noisy)
     with open('tests/test_output_pipeline.pkl', 'rb') as f:
         pipe_test_output = pickle.load(f)
-    assert np.array_equal(pipe_transformed, pipe_test_output)
+    npt.assert_array_almost_equal(pipe_transformed, pipe_test_output,
+                                         decimal=15)
